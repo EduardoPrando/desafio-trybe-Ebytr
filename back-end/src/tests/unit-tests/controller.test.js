4764980282e50ce38cc0e6fae2024be9d55271ce
Eduardo Luiz Prando <eduardoprando159@hotmail.com>
2022-02-15 16:50:45 -0300

    add: newTask controller

---
+++ back-end/src/tests/unit-tests/controller.test.js
@@ -0,0 +1,55 @@
+const Joi = require('@hapi/joi');
+const sinon = require('sinon');
+const { expect } = require('chai');
+const taskController = require;
+
+const taskSchema = Joi.object({
+  task: Joi.string().min(3).required(),
+});
+
+const testedTaskErrorOne = {
+  name: '',
+};
+
+const testedTaskErrorTwo = {
+  name: 'as',
+};
+
+const validTestOne = {
+  name: 'Sinuca',
+};
+
+const validResponse = {
+  _id: 1,
+  taskId: '61fc08f8f91762802a95516c',
+  name: 'Sinuca',
+}
+
+
+describe('Testing task controller:', () => {
+  describe('creatingNewTaskController func', () => {
+    describe('valid data', () => {
+      const req = {};
+      const res = {};
+
+      before(() => {
+        req.body = {
+          name: validTestOne.name,
+        };
+
+        res.status = sinon.stub().returns(res);
+        res.json = sinon.stub().returns();
+        sinon.stub(taskController, 'createNewTaskController').resolves(validResponse);
+      });
+
+      after(() => {
+        taskController.createNewTaskController.restore();
+      });
+
+      it('should create a new task whit status 201', async () => {
+        await
+      })
+    })
+  })
+})
+// createNewTaskController
